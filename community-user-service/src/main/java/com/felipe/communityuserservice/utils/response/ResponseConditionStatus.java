a7e48530ddebb43939e956ff91e3fcc654590bd7
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-05 22:48:55 -0300

    feat: add response utils

---
+++ community-user-service/src/main/java/com/felipe/communityuserservice/utils/response/ResponseConditionStatus.java
@@ -0,0 +1,16 @@
+package com.felipe.communityuserservice.utils.response;
+
+public enum ResponseConditionStatus {
+  SUCCESS("Success"),
+  ERROR("Error");
+
+  private final String value;
+
+  ResponseConditionStatus(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return this.value;
+  }
+}
