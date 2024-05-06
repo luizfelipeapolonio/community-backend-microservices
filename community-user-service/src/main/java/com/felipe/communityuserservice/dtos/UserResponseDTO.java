1cc1afb65e04ba435e861e75e49e9bc279877612
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-05 22:47:58 -0300

    feat: add UserResponseDTO

---
+++ community-user-service/src/main/java/com/felipe/communityuserservice/dtos/UserResponseDTO.java
@@ -0,0 +1,17 @@
+package com.felipe.communityuserservice.dtos;
+
+import com.felipe.communityuserservice.models.User;
+
+import java.time.LocalDateTime;
+
+public record UserResponseDTO(
+  String id,
+  String name,
+  String email,
+  LocalDateTime createdAt,
+  LocalDateTime updatedAt
+) {
+  public UserResponseDTO(User user) {
+    this(user.getId(), user.getName(), user.getEmail(), user.getCreatedAt(), user.getUpdatedAt());
+  }
+}
