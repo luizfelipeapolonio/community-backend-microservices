77ef86541aea47553f10d97aa27ba7ea5a5f9d8f
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-05 22:47:31 -0300

    feat: add fields validation

--- community-user-service/src/main/java/com/felipe/communityuserservice/dtos/UserRegisterDTO.java
+++ community-user-service/src/main/java/com/felipe/communityuserservice/dtos/UserRegisterDTO.java
@@ -1,4 +1,23 @@
 package com.felipe.communityuserservice.dtos;
 
-public record UserRegisterDTO(String name, String email, String password) {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public record UserRegisterDTO(
+  @NotNull(message = "O nome é obrigatório")
+  @NotBlank(message = "O nome não deve estar em branco")
+  @Length(min = 1, max = 100, message = "O nome não deve ter mais de 100 caracteres")
+  String name,
+
+  @NotNull(message = "O e-mail é obrigatório")
+  @NotBlank(message = "O e-mail não deve estar em branco")
+  @Email(message = "O e-mail deve ser e-email válido")
+  String email,
+
+  @NotNull(message = "A senha é obrigatória")
+  @NotBlank(message = "A senha não deve estar em branco")
+  @Length(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+  String password
+) {}
