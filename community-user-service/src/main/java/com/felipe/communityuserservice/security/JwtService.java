17bb133039e9c435fe636f42f5f7f62421dbcfe6
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-10 22:43:33 -0300

    improvement: adjust jwt exception handling

--- community-user-service/src/main/java/com/felipe/communityuserservice/security/JwtService.java
+++ community-user-service/src/main/java/com/felipe/communityuserservice/security/JwtService.java
@@ -3,6 +3,8 @@ package com.felipe.communityuserservice.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,25 +27,33 @@ public class JwtService {
   private RSAPrivateKey privateKey;
 
   public String generateToken(UserPrincipal userPrincipal) {
-    Algorithm algorithm = Algorithm.RSA256(this.publicKey, this.privateKey);
-    return JWT.create()
-      .withIssuer("community-user-service")
-      .withSubject(userPrincipal.getUsername())
-      .withExpiresAt(this.generateExpirationDate())
-      .withClaim("userId", userPrincipal.getUser().getId())
-      .sign(algorithm);
+    try {
+      Algorithm algorithm = Algorithm.RSA256(this.publicKey, this.privateKey);
+      return JWT.create()
+        .withIssuer("community-user-service")
+        .withSubject(userPrincipal.getUsername())
+        .withExpiresAt(this.generateExpirationDate())
+        .withClaim("userId", userPrincipal.getUser().getId())
+        .sign(algorithm);
+    } catch(JWTCreationException | IllegalArgumentException e) {
+      throw new JWTCreationException("Ocorreu um erro interno do servidor", e);
+    }
   }
 
   public Map<String, String> validateToken(String token) {
-    Algorithm algorithm = Algorithm.RSA256(this.publicKey, this.privateKey);
-    JWTVerifier verifier = JWT.require(algorithm).withIssuer("community-user-service").build();
-    DecodedJWT decodedJWT = verifier.verify(token);
+    try {
+      Algorithm algorithm = Algorithm.RSA256(this.publicKey, this.privateKey);
+      JWTVerifier verifier = JWT.require(algorithm).withIssuer("community-user-service").build();
+      DecodedJWT decodedJWT = verifier.verify(token);
 
-    Map<String, String> jwtClaims = new HashMap<>(2);
-    jwtClaims.put("email", decodedJWT.getSubject());
-    jwtClaims.put("userId", decodedJWT.getClaim("userId").asString());
+      Map<String, String> jwtClaims = new HashMap<>(2);
+      jwtClaims.put("email", decodedJWT.getSubject());
+      jwtClaims.put("userId", decodedJWT.getClaim("userId").asString());
 
-    return jwtClaims;
+      return jwtClaims;
+    } catch(JWTVerificationException e) {
+      throw new JWTVerificationException("O token de acesso fornecido expirou, foi revogado ou é inválido", e);
+    }
   }
 
   private Instant generateExpirationDate() {
