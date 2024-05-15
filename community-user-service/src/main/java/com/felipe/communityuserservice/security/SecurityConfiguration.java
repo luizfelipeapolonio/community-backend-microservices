e383dc75dc6cb0a1000c9fc8274218a57369269b
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-14 21:50:09 -0300

    refactor: remove test authorization rule

--- community-user-service/src/main/java/com/felipe/communityuserservice/security/SecurityConfiguration.java
+++ community-user-service/src/main/java/com/felipe/communityuserservice/security/SecurityConfiguration.java
@@ -30,7 +30,6 @@ public class SecurityConfiguration {
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(authorize -> authorize
         .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/validate").permitAll()
-        .requestMatchers("/api/users/test").permitAll()
         .anyRequest().authenticated())
       .addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class)
       .build();
