50c77524796da54df793179900aa78906e6702e7
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-16 21:23:14 -0300

    feat: implement exception handling in the filter

--- community-user-service/src/main/java/com/felipe/communityuserservice/security/SecurityFilter.java
+++ community-user-service/src/main/java/com/felipe/communityuserservice/security/SecurityFilter.java
@@ -4,11 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -16,22 +18,28 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
 
   private final AuthService authService;
+  private final HandlerExceptionResolver resolver;
 
-  public SecurityFilter(AuthService authService) {
+  public SecurityFilter(AuthService authService, @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
     this.authService = authService;
+    this.resolver = resolver;
   }
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-    String email = request.getHeader("email");
-    String userId = request.getHeader("userId");
+    try {
+      String email = request.getHeader("email");
+      String userId = request.getHeader("userId");
 
-    if(email != null && userId != null) {
-      UserDetails userDetails = this.authService.loadUserByUsername(email);
-      var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-      SecurityContextHolder.getContext().setAuthentication(auth);
+      if(email != null && userId != null) {
+        UserDetails userDetails = this.authService.loadUserByUsername(email);
+        var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+      }
+
+      filterChain.doFilter(request, response);
+    } catch(Exception exception) {
+      this.resolver.resolveException(request, response, null, exception);
     }
-
-    filterChain.doFilter(request, response);
   }
 }
