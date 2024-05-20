30895d5eda30b1f9f85d3b8cc5032298a37bb941
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-20 18:30:20 -0300

    refactor: rename and adjust upload storage configuration

--- community-upload-service/src/main/java/com/felipe/communityuploadservice/system/StorageInitializer.java
+++ community-upload-service/src/main/java/com/felipe/communityuploadservice/system/StorageInitializer.java
@@ -1,20 +1,20 @@
 package com.felipe.communityuploadservice.system;
 
-import com.felipe.communityuploadservice.services.StorageService;
+import com.felipe.communityuploadservice.system.config.StorageConfiguration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StorageInitializer implements CommandLineRunner {
 
-  private final StorageService storageService;
+  private final StorageConfiguration storageConfiguration;
 
-  public StorageInitializer(StorageService storageService) {
-    this.storageService = storageService;
+  public StorageInitializer(StorageConfiguration storageConfiguration) {
+    this.storageConfiguration = storageConfiguration;
   }
 
   @Override
   public void run(String... args) throws Exception {
-   this.storageService.init();
+   this.storageConfiguration.init();
   }
 }
--- community-upload-service/src/main/java/com/felipe/communityuploadservice/system/config/StorageConfiguration.java
+++ community-upload-service/src/main/java/com/felipe/communityuploadservice/system/config/StorageConfiguration.java
@@ -1,20 +1,19 @@
-package com.felipe.communityuploadservice.services;
+package com.felipe.communityuploadservice.system.config;
 
 import com.felipe.communityuploadservice.exceptions.UploadDirectoryInitializationException;
-import com.felipe.communityuploadservice.system.config.StorageProperties;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Service
-public class StorageService {
+@Component
+public class StorageConfiguration {
 
   private final Path imageStorageLocation;
 
-  public StorageService(StorageProperties storageProperties) {
+  public StorageConfiguration(StorageProperties storageProperties) {
     this.imageStorageLocation = Paths.get(storageProperties.getUploadDir())
       .toAbsolutePath().normalize();
   }
@@ -22,7 +21,9 @@ public class StorageService {
   public void init() {
     try {
       if(Files.exists(this.imageStorageLocation)) return;
-      Files.createDirectory(this.imageStorageLocation);
+      Path root = Files.createDirectory(this.imageStorageLocation);
+      Files.createDirectory(Paths.get(root.toString(), "user"));
+      Files.createDirectory(Paths.get(root.toString(), "post"));
     } catch(IOException e) {
       System.out.println("Erro ao criar diretório de upload -> " + e.getMessage());
       throw new UploadDirectoryInitializationException("Não foi possível criar diretório de upload", e);
