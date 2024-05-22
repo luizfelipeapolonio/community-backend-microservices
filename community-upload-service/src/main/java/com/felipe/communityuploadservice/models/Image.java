7f46d06cdd7ce6fd56107e672f17dfbb03a21690
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-21 22:19:37 -0300

    improvement: add size field

--- community-upload-service/src/main/java/com/felipe/communityuploadservice/models/Image.java
+++ community-upload-service/src/main/java/com/felipe/communityuploadservice/models/Image.java
@@ -1,9 +1,85 @@
 package com.felipe.communityuploadservice.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
 
-//@Entity
-//@Table(name = "image")
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "image")
 public class Image {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  private String name;
+
+  // TODO: Colocar field "size"
+  private long size;
+
+  @Column(name = "post_id")
+  private String postId;
+
+  @Column(name = "user_id")
+  private String userId;
+
+  @CreationTimestamp
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  public Image() {}
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public long getSize() {
+    return this.size;
+  }
+
+  public void setSize(long size) {
+    this.size = size;
+  }
+
+  public String getPostId() {
+    return this.postId;
+  }
+
+  public void setPostId(String postId) {
+    this.postId = postId;
+  }
+
+  public String getUserId() {
+    return this.userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return this.createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
 }
