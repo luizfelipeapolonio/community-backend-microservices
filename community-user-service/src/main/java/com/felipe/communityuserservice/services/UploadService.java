5a2c8d2b3992ca09e2b21f4aa53eed4dae40c054
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-27 21:52:41 -0300

    feat: implement upload service

---
+++ community-user-service/src/main/java/com/felipe/communityuserservice/services/UploadService.java
@@ -0,0 +1,39 @@
+package com.felipe.communityuserservice.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.felipe.communityuserservice.clients.UploadClient;
+import com.felipe.communityuserservice.dtos.UploadDTO;
+import com.felipe.communityuserservice.dtos.UploadResponseDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadService {
+
+  private final UploadClient uploadClient;
+  private final ObjectMapper objectMapper;
+
+  public UploadService(UploadClient uploadClient, ObjectMapper objectMapper) {
+    this.uploadClient = uploadClient;
+    this.objectMapper = objectMapper;
+  }
+
+  public UploadResponseDTO upload(UploadDTO uploadDTO, MultipartFile image) {
+    try {
+      String jsonUploadDTO = this.objectMapper.writeValueAsString(uploadDTO);
+      return this.uploadClient.uploadImage(jsonUploadDTO, image);
+    } catch(JsonProcessingException e) {
+      throw new RuntimeException("Não foi possível converter para JSON");
+    }
+  }
+
+  public <T> T convertJsonStringToObject(String jsonString, Class<T> targetClass) {
+    try {
+      return this.objectMapper.readValue(jsonString, targetClass);
+    } catch(JsonProcessingException e) {
+      // TODO: criar uma exceção personalizada
+      throw new RuntimeException("Não foi possível processar os dados da requisição");
+    }
+  }
+}
