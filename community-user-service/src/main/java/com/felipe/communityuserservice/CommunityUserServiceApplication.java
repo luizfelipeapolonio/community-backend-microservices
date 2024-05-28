1baab3d28ce58ef2f9f8744fd062cae3fead8da9
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-27 21:47:21 -0300

    feat: Enable feign clients

--- community-user-service/src/main/java/com/felipe/communityuserservice/CommunityUserServiceApplication.java
+++ community-user-service/src/main/java/com/felipe/communityuserservice/CommunityUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.felipe.communityuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CommunityUserServiceApplication {
 
 	public static void main(String[] args) {
