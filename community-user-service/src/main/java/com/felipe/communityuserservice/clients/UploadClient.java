73d57acf0937bf28f47ba29db9111711efd45706
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-27 21:41:40 -0300

    feat: add upload client

---
+++ community-user-service/src/main/java/com/felipe/communityuserservice/clients/UploadClient.java
@@ -0,0 +1,15 @@
+package com.felipe.communityuserservice.clients;
+
+import com.felipe.communityuserservice.dtos.UploadResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@FeignClient(name = "COMMUNITY-UPLOAD-SERVICE", path = "/api/uploads")
+public interface UploadClient {
+
+  @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+  UploadResponseDTO uploadImage(@RequestPart("data") String uploadDTO, @RequestPart("image") MultipartFile image);
+}
