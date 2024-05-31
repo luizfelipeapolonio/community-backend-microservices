3da11ed2ae24599c74c1245cba20deb360748c70
Luiz Felipe <luizfelipe0527@gmail.com>
2024-05-31 19:51:08 -0300

    refactor: adjust save image controller

--- community-upload-service/src/main/java/com/felipe/communityuploadservice/controllers/UploadController.java
+++ community-upload-service/src/main/java/com/felipe/communityuploadservice/controllers/UploadController.java
@@ -1,11 +1,13 @@
 package com.felipe.communityuploadservice.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.felipe.communityuploadservice.dtos.UploadDTO;
 import com.felipe.communityuploadservice.models.Image;
 import com.felipe.communityuploadservice.services.UploadService;
-import com.felipe.communityuploadservice.utils.response.CustomResponseBody;
-import com.felipe.communityuploadservice.utils.response.ResponseConditionStatus;
+import com.felipe.communityuploadservice.system.config.StorageProperties;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -18,24 +20,29 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 
   private final UploadService uploadService;
+  private final ObjectMapper objectMapper;
+  private final StorageProperties storageProperties;
 
-  public UploadController(UploadService uploadService) {
+  public UploadController(UploadService uploadService, ObjectMapper objectMapper, StorageProperties storageProperties) {
     this.uploadService = uploadService;
+    this.objectMapper = objectMapper;
+    this.storageProperties = storageProperties;
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public CustomResponseBody<Image> save(
-    @RequestPart("image") MultipartFile image,
-    @RequestPart("data") UploadDTO uploadDTO
-  ) {
-    Image uploadedImage = this.uploadService.save(uploadDTO, image);
+  public Image save( @RequestPart("data") String jsonUploadDTO, @RequestPart("image") MultipartFile image) {
+    try {
+      UploadDTO uploadDTO = this.objectMapper.readValue(jsonUploadDTO, UploadDTO.class);
+      return this.uploadService.save(uploadDTO, image);
+    } catch(JsonProcessingException e) {
+      throw new RuntimeException(e.getMessage());
+    }
+  }
 
-    CustomResponseBody<Image> response = new CustomResponseBody<>();
-    response.setStatus(ResponseConditionStatus.SUCCESS);
-    response.setCode(HttpStatus.CREATED);
-    response.setMessage("Image salva com successo");
-    response.setData(uploadedImage);
-    return response;
+  @GetMapping("/properties")
+  @ResponseStatus(HttpStatus.OK)
+  public String getUploadProperties() {
+    return this.storageProperties.getUploadDir();
   }
 }
