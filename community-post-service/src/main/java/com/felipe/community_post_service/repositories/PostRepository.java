9668b36774028901b69cdafcaa9819ce12b46ece
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-18 20:33:55 -0300

    feat: add method to get all posts from a specific user

--- community-post-service/src/main/java/com/felipe/community_post_service/repositories/PostRepository.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/repositories/PostRepository.java
@@ -1,7 +1,10 @@
 package com.felipe.community_post_service.repositories;
 
 import com.felipe.community_post_service.models.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, String> {
+  Page<Post> findAllByOwnerId(String ownerId, Pageable pagination);
 }
