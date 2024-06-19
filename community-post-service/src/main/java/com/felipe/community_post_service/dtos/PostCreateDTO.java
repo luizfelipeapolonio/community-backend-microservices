452a03a693d485ddd6a1bac60bcb23a593228a1a
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-19 17:57:26 -0300

    refactor: adjust validation

--- community-post-service/src/main/java/com/felipe/community_post_service/dtos/PostCreateDTO.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/dtos/PostCreateDTO.java
@@ -1,23 +1,21 @@
 package com.felipe.community_post_service.dtos;
 
-import com.felipe.community_post_service.dtos.constraints.Creation;
-import com.felipe.community_post_service.dtos.constraints.Update;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Length;
 
-public record PostCreateAndUpdateDTO(
-  @NotNull(message = "O título é obrigatório", groups = Creation.class)
-  @NotBlank(message = "O título não deve estar em branco", groups = Creation.class)
-  @Length(max = 80, message = "O título não deve ter mais de 80 caracteres", groups = {Creation.class, Update.class})
+public record PostCreateDTO(
+  @NotNull(message = "O título é obrigatório")
+  @NotBlank(message = "O título não deve estar em branco")
+  @Length(max = 80, message = "O título não deve ter mais de 80 caracteres")
   String title,
 
-  @NotNull(message = "O conteúdo é obrigatório", groups = Creation.class)
-  @NotBlank(message = "O conteúdo não deve estar em branco", groups = Creation.class)
+  @NotNull(message = "O conteúdo é obrigatório")
+  @NotBlank(message = "O conteúdo não deve estar em branco")
   String content,
 
-  @NotNull(message = "As tags são obrigatórias", groups = Creation.class)
-  @Size(min = 1, max = 10, message = "Deve ter pelo menos 1 tag e no máximo 10 tags", groups = {Creation.class, Update.class})
+  @NotNull(message = "As tags são obrigatórias")
+  @Size(min = 1, max = 10, message = "Deve ter pelo menos 1 tag e no máximo 10 tags")
   String[] tags
 ) {}
