b2564a3196831a8025b8f1f61d9bb8a4c73f1a7b
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-19 21:27:29 -0300

    feat: add find all posts by owner id method

--- community-post-service/src/main/java/com/felipe/community_post_service/repositories/PostRepository.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/repositories/PostRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, String> {
   Page<Post> findAllByOwnerId(String ownerId, Pageable pagination);
+  List<Post> findAllByOwnerId(String ownerId);
 }
