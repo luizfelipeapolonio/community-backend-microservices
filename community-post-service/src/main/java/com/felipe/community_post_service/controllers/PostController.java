9536ff1db83824cbc036820dc0ed88451fa3997e
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-21 22:51:50 -0300

    feat: implement insert comment in a post function

--- community-post-service/src/main/java/com/felipe/community_post_service/controllers/PostController.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/controllers/PostController.java
@@ -1,15 +1,20 @@
 package com.felipe.community_post_service.controllers;
 
+import com.felipe.community_post_service.dtos.CommentCreateAndUpdateDTO;
+import com.felipe.community_post_service.dtos.CommentResponseDTO;
 import com.felipe.community_post_service.dtos.PostCreateDTO;
 import com.felipe.community_post_service.dtos.PostPageResponseDTO;
 import com.felipe.community_post_service.dtos.PostResponseDTO;
 import com.felipe.community_post_service.dtos.PostUpdateDTO;
 import com.felipe.community_post_service.dtos.mappers.PostMapper;
+import com.felipe.community_post_service.models.Comment;
 import com.felipe.community_post_service.models.Post;
+import com.felipe.community_post_service.services.CommentService;
 import com.felipe.community_post_service.services.PostService;
 import com.felipe.community_post_service.services.UploadService;
 import com.felipe.community_post_service.util.response.CustomResponseBody;
 import com.felipe.community_post_service.util.response.ResponseConditionStatus;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,11 +42,13 @@ public class PostController {
   private final PostService postService;
   private final UploadService uploadService;
   private final PostMapper postMapper;
+  private final CommentService commentService;
 
-  public PostController(PostService postService, UploadService uploadService, PostMapper postMapper) {
+  public PostController(PostService postService, UploadService uploadService, PostMapper postMapper, CommentService commentService) {
     this.postService = postService;
     this.uploadService = uploadService;
     this.postMapper = postMapper;
+    this.commentService = commentService;
   }
 
   @PostMapping
@@ -177,6 +185,24 @@ public class PostController {
     response.setCode(HttpStatus.OK);
     response.setMessage("Todos os posts do usuário de id: '" + userId + "'");
     response.setData(postPageResponseDTO);
+    return response;
+  }
+
+  @PostMapping("/{postId}/comments")
+  @ResponseStatus(HttpStatus.CREATED)
+  public CustomResponseBody<CommentResponseDTO> insertComment(
+    @RequestHeader("userId") String userId,
+    @PathVariable String postId,
+    @RequestBody @Valid CommentCreateAndUpdateDTO commentDTO
+  ) {
+    Comment comment = this.commentService.insertComment(userId, postId, commentDTO);
+    CommentResponseDTO commentResponseDTO = new CommentResponseDTO(comment);
+
+    CustomResponseBody<CommentResponseDTO> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.SUCCESS);
+    response.setCode(HttpStatus.CREATED);
+    response.setMessage("Comentário inserido com sucesso no post de id: '" + postId +"'");
+    response.setData(commentResponseDTO);
     return response;
   }
 }
---
+++ community-post-service/src/main/java/com/felipe/community_post_service/services/CommentService.java
@@ -0,0 +1,38 @@
+package com.felipe.community_post_service.services;
+
+import com.felipe.community_post_service.clients.UserClient;
+import com.felipe.community_post_service.dtos.CommentCreateAndUpdateDTO;
+import com.felipe.community_post_service.models.Comment;
+import com.felipe.community_post_service.models.Post;
+import com.felipe.community_post_service.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class CommentService {
+
+  private final CommentRepository commentRepository;
+  private final PostService postService;
+  private final UserClient userClient;
+
+  public CommentService(CommentRepository commentRepository, PostService postService, UserClient userClient) {
+    this.commentRepository = commentRepository;
+    this.postService = postService;
+    this.userClient = userClient;
+  }
+
+  public Comment insertComment(String userId, String postId, CommentCreateAndUpdateDTO commentDTO) {
+    Post post = this.postService.getById(postId);
+    Map<String, String> userInfos = this.userClient.getUserInfos(userId);
+
+    Comment newComment = new Comment();
+    newComment.setUserId(userId);
+    newComment.setUsername(userInfos.get("username"));
+    newComment.setProfileImage(userInfos.get("profileImage"));
+    newComment.setContent(commentDTO.content());
+    newComment.setPost(post);
+
+    return this.commentRepository.save(newComment);
+  }
+}
