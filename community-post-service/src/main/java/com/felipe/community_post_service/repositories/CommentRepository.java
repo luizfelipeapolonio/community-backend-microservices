a7bd98d40ee08ef2e57636348307e99967d0db86
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-22 22:58:03 -0300

    feat: add method to get all post comments

--- community-post-service/src/main/java/com/felipe/community_post_service/repositories/CommentRepository.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/repositories/CommentRepository.java
@@ -1,7 +1,14 @@
 package com.felipe.community_post_service.repositories;
 
 import com.felipe.community_post_service.models.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, String> {
+
+  @Query(value = "SELECT c FROM Comment c WHERE c.post.id = :postId ORDER BY c.createdAt DESC")
+  Page<Comment> findAllByPostId(@Param("postId") String postId, Pageable pageable);
 }
