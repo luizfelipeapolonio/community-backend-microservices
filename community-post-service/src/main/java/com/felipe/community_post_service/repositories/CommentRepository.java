0d7d45356f5be0c30ef67f19564111a98cab43c0
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-24 00:15:28 -0300

    feat: add method to find a comment by id and post id

--- community-post-service/src/main/java/com/felipe/community_post_service/repositories/CommentRepository.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/repositories/CommentRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, String> {
 
   @Query(value = "SELECT c FROM Comment c WHERE c.post.id = :postId ORDER BY c.createdAt DESC")
   Page<Comment> findAllByPostId(@Param("postId") String postId, Pageable pageable);
+
+  @Query(value = "SELECT c FROM Comment c WHERE c.id = :commentId AND c.post.id = :postId")
+  Optional<Comment> findByIdAndPostId(@Param("commentId") String commentId, @Param("postId") String postId);
 }
