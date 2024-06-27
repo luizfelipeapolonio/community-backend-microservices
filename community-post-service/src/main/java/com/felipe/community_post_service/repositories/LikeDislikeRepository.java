62d673638b5bc1f9ac7f7a49eb949ad92bc35a45
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-26 22:16:25 -0300

    feat: add methods to find like or dislike by post id and user id

--- community-post-service/src/main/java/com/felipe/community_post_service/repositories/LikeDislikeRepository.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/repositories/LikeDislikeRepository.java
@@ -2,6 +2,20 @@ package com.felipe.community_post_service.repositories;
 
 import com.felipe.community_post_service.models.LikeDislike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface LikeDislikeRepository extends JpaRepository<LikeDislike, Long> {
+
+  @Query(value = "SELECT ld FROM LikeDislike ld WHERE ld.post.id = :postId AND ld.userId = :userId AND ld.type = :type")
+  Optional<LikeDislike> findByPostIdAndUserIdAndType(
+    @Param("postId") String postId,
+    @Param("userId") String userId,
+    @Param("type") String type
+  );
+
+  @Query(value = "SELECT ld FROM LikeDislike ld WHERE ld.post.id = :postId AND ld.userId = :userId")
+  Optional<LikeDislike> findByPostIdAndUserId(@Param("postId") String postId, @Param("userId") String userId);
 }
