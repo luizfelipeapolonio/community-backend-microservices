e122617d3f84539773195ee2f5fddd508cf70808
Luiz Felipe <luizfelipe0527@gmail.com>
2024-06-30 19:34:55 -0300

    feat: add method to find all posts by title or tags

--- community-post-service/src/main/java/com/felipe/community_post_service/repositories/PostRepository.java
+++ community-post-service/src/main/java/com/felipe/community_post_service/repositories/PostRepository.java
@@ -4,10 +4,15 @@ import com.felipe.community_post_service.models.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, String> {
   Page<Post> findAllByOwnerId(String ownerId, Pageable pagination);
   List<Post> findAllByOwnerId(String ownerId);
+
+  @Query(value = "SELECT p FROM Post p WHERE p.title LIKE %:query% OR p.tags LIKE %:query%")
+  Page<Post> findAllByTitleOrTagsLike(@Param("query") String query, Pageable pageable);
 }
